b5a4aa6aa0e0ed332674787aaf2d512ef43f48ab
paulaotero14 <paula.otero.colmenar@alumnos.upm.es>
2018-05-03 10:21:53 +0200

    p5 nueva

---
+++ models/index.js
@@ -0,0 +1,35 @@
+const Sequelize = require('sequelize');
+
+  const sequelize = new Sequelize("sqLite:quizzes.sqlite", {logging : false});
+
+  sequelize.define('quiz', {
+	  question: {
+		  type: Sequelize.STRING,
+		  unique: {msg: "Ya existe esta pregunta"},
+		  validate: {noEmpty: {msg: "La pregunta no puede estar vacia"}}
+	  },
+	  answer: {
+		  type: Sequelize.STRING,
+		  validate: {noEmpty: {msg: "La respuesta no puede estar vacía."}}
+	  }
+	});
+
+
+	sequelize.sync()
+	.then(() => sequelize.models.quiz.count())
+	.then(count => {
+		if (!count) {
+			return sequelize.models.quiz.bulkCreate([
+				{question: "Capital de Italia", answer: "Roma" },
+				{question: "Capital de Francia", answer: "Paris" },
+				{question: "Capital de España", answer: "Madrid" },
+				{question: "Capital de Portugal", answer: "Lisboa" }
+				
+			]);
+		}
+	})
+	.catch(error => {
+		console.log(error);
+	});
+
+	module.exports = sequelize;
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,6 @@
 var express = require('express');
+const Sequelize = require('sequelize');
+const {models} = require("../models/index.js");
 var router = express.Router();
 
 /* GET home page. */
@@ -9,5 +11,40 @@ router.get('/', function(req, res, next) {
 router.get('/credits', function(req, res, next) {
   res.render('credits', { title: 'Credits' });
 });
+
+router.get('/quizzes', (req, res, next) => {
+  aux = ""
+  response = ""
+  n = 0
+  models.quiz.findAll()
+    .each(quiz => {
+      aux = `Pregunta Número ${quiz.id}: ${quiz.question}`
+      aux = aux.concat(aux)
+      response = JSON.stringify(aux)
+      
+  })
+  .then((response, n) => {
+  
+    response = JSON.stringify(response)
+    
+    console.log("Dentro de then(response) => "+response)
+  
+    res.send('<!DOCTYPE html>'
+      +'<html>'
+      +'<head>'
+      +    '<title>Quizzes</title>'
+      +     '<link rel="stylesheet" href="/stylesheets/style.css" />'
+      + '</head>'
+      + '<body>'
+      
+      +         response
+      + '</body>'
+      + '</html>'
+            )
+  })
+ 
+ 
+ })
+
 
 module.exports = router;
